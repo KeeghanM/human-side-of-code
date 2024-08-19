973664ce4cefff9b9170292f36528baf9efa5c76
Keeghan McGarry <keeghan_m@live.com>
2024-08-19 22:46:17 +0100

    Setup Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    sanity({
+      projectId: import.meta.env.SANITY_PROJECT_ID,
+      dataset: import.meta.env.SANITY_DATASET,
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
