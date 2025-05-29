894cf07f429959b38e5b948f5bb3605ccb4e54c5
Keeghan McGarry <keeghan_m@live.com>
2025-05-29 11:38:02 +0100

    fixed about

--- src/components/pitch-dialog/pitch-dialog.tsx
+++ src/components/pitch-dialog/pitch-dialog.tsx
@@ -24,7 +24,7 @@ export default function PitchDialog({ tabs }: PitchProps) {
       </button>
       <dialog
         ref={ref}
-        className="bg-background-50 text-text-800 p-4 md:p-6 rounded-lg shadow w-[calc(100vw_-_2rem)] max-w-3xl my-auto mx-auto relative"
+        className="bg-background-50 text-text-800 p-4 md:p-6 rounded-lg shadow w-[calc(100vw_-_2rem)] max-w-3xl my-auto mx-auto fixed"
       >
         <button onClick={() => ref.current?.close()} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer">
           <svg
