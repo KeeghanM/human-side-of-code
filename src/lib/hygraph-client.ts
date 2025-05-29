8b1637bf8d26a8d75f0c05633b3cd70dc5fb8b9e
Keeghan McGarry <keeghan_m@live.com>
2025-05-29 13:35:28 +0100

    throttled

--- src/lib/hygraph-client.ts
+++ src/lib/hygraph-client.ts
@@ -1,6 +1,24 @@
-import {GraphQLClient} from "graphql-request";
-export const hygraph = new GraphQLClient(import.meta.env.HYGRAPH_ENDPOINT,{
-    headers: {
-        Authorization: `Bearer ${import.meta.env.HYGRAPH_ACCESS_TOKEN}`,
-    }
+import pThrottle from "p-throttle";
+import {
+  GraphQLClient,
+  type RequestDocument,
+  type Variables,
+} from "graphql-request";
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
+
+const throttle = pThrottle({
+  limit: 5,
+  interval: 1000,
 });
+
+const client = new GraphQLClient(import.meta.env.HYGRAPH_ENDPOINT, {
+  headers: {
+    Authorization: `Bearer ${import.meta.env.HYGRAPH_ACCESS_TOKEN}`,
+  },
+});
+
+export const hygraphRequest = throttle(
+  async <TData = any, TVariables extends Variables = Variables>(
+    query: TypedDocumentNode<TData, TVariables> | RequestDocument,
+  ): Promise<TData> => client.request<TData>(query),
+);
